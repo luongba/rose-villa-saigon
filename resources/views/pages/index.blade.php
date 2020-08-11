7d7d74df36da120e1a0f7fdfa2a0b1e40e77494c
ManhTuICT <gautrangcb91@gmail.com>
2020-08-11 10:24:42 +0700

    update 11.08.2020

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,3 @@
-@extends('master')
   <div class="loading">
     <div class="spinner">
           <i class="fas fa-spinner fa-spin fa-3x fa-fw"></i>
@@ -19,6 +18,7 @@
   <audio id="audio" autoplay>
     <source type="audio/mp3" src="{{asset('public/assets/audio/La vie en rose - Louis Armstrong.mp3')}}">
   </audio>
+@extends('master')
 @section('content')
   <div class="landing landingstyle">
     <div class="innerpage-style1">
@@ -30,8 +30,45 @@
   </div>
 @endsection
 
+<style>
+  #page-wrap {
+    visibility: hidden;
+  }
+ 
+  .loading {
+      background-image: url('images/logo_loading.png');
+      background-repeat: no-repeat;
+      background-position: center;
+      position: absolute;
+      background-size: 20%;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 99999;
+  }
+  .spinner {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-top: -20px;
+      margin-left: -25px;
+      width: 50px;
+      height: 40px;
+      text-align: center;
+      font-size: 10px;
+      color: #fff;
+  }
+.header,
+.content-custom-width {
+    visibility: hidden;
+  }
+</style>
 @section('script')
-
+<script src="{{asset('public/js/curtains.js')}}" type="text/javascript"></script>
+<script src="{{asset('public/js/simple.plane.setup.js')}}" type="text/javascript"></script>
 <script>
       function ready() {
           setTimeout(function(){
@@ -47,6 +84,4 @@
       
       document.addEventListener("DOMContentLoaded", ready);
     </script>
-    <script src="{{asset('public/js/curtains.js')}}" type="text/javascript"></script>
-<script src="{{asset('public/js/simple.plane.setup.js')}}" type="text/javascript"></script>
 @endsection
